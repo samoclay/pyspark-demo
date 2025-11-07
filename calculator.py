f89895e8097c672934bb080056fc31d1fd1b26c7
baloindia2015-art <baloindia2015@gmail.com>
2025-11-07 14:41:30 +0530

    feature/new-vocal

--- calculator.py
+++ calculator.py
@@ -3,4 +3,4 @@ import pyspark
 a = int(input("Enter a number: "))
 b = int(input("Enter another number: "))
 sum = a+b
-print(f"The sum of {a} and {b} is {sum}")
+print(f"The sum of {a} and {b} is {sum}") 
